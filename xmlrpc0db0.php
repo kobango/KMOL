c5960741c7a775bb04f58cd068e3f8e69196be04
Kamil <kamilszkaradnik@gmail.com>
2025-04-26 12:40:10 +0200

    init

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://server848640.nazwa.pl/wordpress/wpn_kmol</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://server848640.nazwa.pl/wordpress/wpn_kmol/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://server848640.nazwa.pl/wordpress/wpn_kmol/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://server848640.nazwa.pl/wordpress/wpn_kmol/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://server848640.nazwa.pl/wordpress/wpn_kmol/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://server848640.nazwa.pl/wordpress/wpn_kmol/index.php?rest_route=/" />
+			</apis>
+	</service>
+</rsd>
+	
